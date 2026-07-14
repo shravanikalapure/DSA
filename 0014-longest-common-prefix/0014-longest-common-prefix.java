33ecd5a7f9a6fc2a31354c937e273d5f1036dc13
Shravani Kalapure <kalapureshravani@gmail.com>
2026-07-14 22:00:40 +0530

    Time: 0 ms (100%), Space: 43.1 MB (67.9%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String ans = strs[0];
+        for(int i = 0; i < strs.length; i++){
+            while(!strs[i].startsWith(ans)){
+                ans = ans.substring(0, ans.length() - 1);
+
+                if(ans.isEmpty()){
+                    return "";
+                }
+            }
+        }
+        return ans;
+    }
+}
